e96c310835dda38b851a9a539ddc63803fe002f9
Will Thomas <30wthomas@gmail.com>
2023-12-20 21:43:30 -0600

    better counter

--- learner.py
+++ learner.py
@@ -14,7 +14,9 @@ def get_char():
 
 # charList = ["n", "r", "t", "a"]
 charList = ["i", "e", "s", "o"]
-t_end = time.time() - 1
+count = 1
+base_time = time.time()
+timer = time.time()
 
 while True:
     clear_screen()
@@ -22,13 +24,14 @@ while True:
     pick2 = random.choice(charList)
     pick3 = random.choice(charList)
     pick4 = random.choice(charList)
-    t_start = time.time()
-    print(pick1 + pick2 + pick3 + pick4 + f"\t{t_end - t_start}")
+    print(
+        pick1 + pick2 + pick3 + pick4 + f"\tWPM: {count/((timer - base_time + 1)/60)}"
+    )
     char1 = get_char()
     char2 = get_char()
     char3 = get_char()
     char4 = get_char()
-    t_end = time.time()
+    timer = time.time()
     while char1 != pick1 or char2 != pick2 or char3 != pick3 or char4 != pick4:
         if char1 == "!" and char2 == "!" and char3 == "!" and char4 == "!":
             exit(1)
@@ -37,4 +40,5 @@ while True:
         char2 = get_char()
         char3 = get_char()
         char4 = get_char()
-        t_end = time.time()
+        timer = time.time()
+    count += 1
