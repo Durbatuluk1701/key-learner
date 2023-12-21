72ff8f2f3b5b5f2beef97f2db60278c5eac89303
Will Thomas <30wthomas@gmail.com>
2023-12-21 17:18:53 -0600

    adding window

--- learner.py
+++ learner.py
@@ -12,33 +12,36 @@ def get_char():
     return msvcrt.getch().decode("utf-8")
 
 
+WINDOW = 5
+
 charList = ["n", "r", "t", "a"]
 charList += ["i", "e", "s", "o"]
-count = 1
-base_time = time.time()
-timer = time.time()
+# count = 1
+# base_time = time.time()
+# timer = time.time()
+
+targ_buf = [random.choice(charList) for i in range(WINDOW)]
+
+
+def pbuf(l):
+    for i in l:
+        print(i, end="")
+    # print(f"\tWPM: {60 * count/(timer - base_time + 1)}")
+
 
 while True:
     clear_screen()
-    pick1 = random.choice(charList)
-    pick2 = random.choice(charList)
-    pick3 = random.choice(charList)
+    targ_buf.append(random.choice(charList))
     pick4 = random.choice(charList)
-    print(
-        pick1 + pick2 + pick3 + pick4 + f"\tWPM: {count/((timer - base_time + 1)/60)}"
-    )
-    char1 = get_char()
-    char2 = get_char()
-    char3 = get_char()
-    char4 = get_char()
-    timer = time.time()
-    while char1 != pick1 or char2 != pick2 or char3 != pick3 or char4 != pick4:
-        if char1 == "!" and char2 == "!" and char3 == "!" and char4 == "!":
+    print(targ_buf)
+    # pbuf(targ_buf)
+    char = get_char()
+    # timer = time.time()
+    while char != targ_buf[0]:
+        if char == "!":
             exit(1)
         print("WRONG!")
-        char1 = get_char()
-        char2 = get_char()
-        char3 = get_char()
-        char4 = get_char()
-        timer = time.time()
-    count += 1
+        char = get_char()
+        # timer = time.time()
+    targ_buf.pop(0)
+    # count += 1
